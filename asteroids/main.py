3ccf4e5c1ca64dbd2fca2e4562e105f0e263f900
Spicy-Jam <javandyke1997@gmail.com>
2025-10-14 16:53:28 -0600

    fixed infinite loop and exit event

--- asteroids/main.py
+++ asteroids/main.py
@@ -4,12 +4,13 @@ from constants import *
 
 
 def main():
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    i = 0
-    while i < 1:
+  
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
         screen.fill(0)
         pygame.display.flip()
 
